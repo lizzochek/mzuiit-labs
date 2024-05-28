d69df0d1fc2fc9836c40b40db6a31dc28923e549
Yelyzaveta Dolhova <liza.dolhova@gforces.pl>
2024-05-28 17:13:24 +0300

    Lab4 - modified server

--- lab3-server/server.js
+++ lab3-server/server.js
@@ -1,6 +1,23 @@
 "use strict";
 const winston = require("winston");
 const express = require("express");
+const Prometheus = require("prom-client");
+
+Prometheus.collectDefaultMetrics({ timeout: 1000 });
+
+// Custom metrics
+const totalCalls = new Prometheus.Counter({
+  name: "calls_total",
+  help: "Total number of calls",
+  labelNames: ["result"],
+});
+
+const delays = new Prometheus.Histogram({
+  name: "delays_ms",
+  help: "Delay of the request in ms",
+  labelNames: ["method", "route", "code"],
+  buckets: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+});
 
 const logger = winston.createLogger({
   level: "info",
@@ -23,6 +40,8 @@ app.get("/", (req, res, next) => {
 app.get("/random", (req, res, next) => {
   const result = Math.floor(Math.random() * 100) > 30;
   const transactionTime = Math.round(Math.random() * 100);
+  totalCalls.inc({ result });
+  delays.labels(req.method, req.route.path, res.statusCode).observe(transactionTime);
   if (!result) {
     next(new Error("Sorry! Your number is less than 30."));
     logger.error("Error happened", { errCode: "1", transactionTime });
@@ -35,12 +54,17 @@ app.get("/random", (req, res, next) => {
   }
 });
 
+app.get("/metrics", (req, res) => {
+  res.set("Content-Type", Prometheus.register.contentType);
+  Prometheus.register.metrics().then((data) => res.send(data));
+});
+
 app.use((err, req, res, next) => {
   res.statusCode = 500;
   res.json({ error: err.message });
   next();
 });
 
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Lab3 app listening on port ${port}!`);
 });
